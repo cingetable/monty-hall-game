07127f3b7a35808b14573914ed2652e933826b0f
bazdu <bazdukovvalentin@gmail.com>
2022-10-30 22:09:15 +1000

    add fields and restart logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+var gameCount = document.getElementById('game_count');
+var doorChangedCount = document.getElementById('door_changed_count');
+var doorNotChangedCount = document.getElementById('door_not_changed_count');
+var doorChangedWins = document.getElementById('door_changed_win');
+var doorNotChangedWins = document.getElementById('door_not_changed_win');
+var doorChangedLose = document.getElementById('door_changed_lose');
+var doorNotChangedLose = document.getElementById('door_changed_lose');
+
+
 function open_door(buttonId) {
     document.getElementById(buttonId).style.backgroundImage = "url(../img/open_door.png)";
+}
+function restart(buttonId) {
+    gameCount.textContent = '0';
+    doorChangedCount.textContent = '0';
+    doorNotChangedCount.textContent = '0';
+    doorChangedWins.textContent = '0';
+    doorNotChangedWins.textContent = '0';
+    doorChangedLose.textContent = '0';
+    doorNotChangedLose.textContent = '0';
 }
